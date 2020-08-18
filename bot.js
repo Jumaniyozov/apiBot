6dcbbad1d7e6a27679e5164e7d2c2ae524541d49
Jumaniyozov <JumaniyozovSE@gmail.com>
2020-08-18 21:07:57 +0500

    initial commit

---
+++ bot.js
@@ -0,0 +1,11 @@
+const Telegraf = require('telegraf')
+// const express = require('express')
+
+const bot = new Telegraf('1343858667:AAErXGB0fIkwn2EpEHQMDbJkLPmIXy3_O08')
+
+const axios = require('axios');
+
+bot.command()
+
+
+bot.launch();
